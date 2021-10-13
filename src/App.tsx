8793f929309fafba74e64ee7cde9efc06a42b121
carlos roberto <carlosrobertounopar01@gmail.com>
2021-10-13 14:14:41 -0300

    Inicio

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { GlobalStyle } from "./styles/global";
+
+
+export function App() {
+  return (
+    <div className="App">
+      <h1>helle word</h1>
+      <GlobalStyle/>
+    </div>
+  );
+}
+
+
