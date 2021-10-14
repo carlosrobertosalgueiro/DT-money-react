b06d80834cfceed1b0aa6d301843f6568a48069a
carlos roberto <carlosrobertounopar01@gmail.com>
2021-10-14 01:54:15 -0300

    header inserido

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 
 export function App() {
   return (
-    <div className="App">
-      <h1>helle word</h1>
+    <>
+      <Header/>
       <GlobalStyle/>
-    </div>
+    </>
   );
 }
 
