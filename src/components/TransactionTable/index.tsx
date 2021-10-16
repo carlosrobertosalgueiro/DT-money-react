f072674c434b802e9c0896114d39cf1a2f41cd5a
carlos roberto <carlosrobertounopar01@hotmail.com>
2021-10-16 00:04:18 -0300

    miragejs e axios configurados

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
+import { api } from "../../Services/api";
 import { Container } from "./style";
 
 export function TransactionTable(){
+
+    useEffect( ()=> {
+        api.get('transactions')
+        .then(response => console.log(response.data))
+    },[])
 
         return(
             <Container>
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import {App} from './App';
 
+createServer({
+  routes(){
+    this.namespace = 'api';
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'transactions',
+          amount:400,
+          type: 'deposit',
+          category: 'Foot',
+          createDat: new Date()
+
+        }
+      ]
+    }) 
+
+  }
+
+})
 
 ReactDOM.render(
   <React.StrictMode>
