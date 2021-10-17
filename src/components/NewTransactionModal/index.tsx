0b66a95cf9085a148f57ac227fc8e91d74394c4f
carlos roberto <carlosrobertounopar01@hotmail.com>
2021-10-17 00:32:51 -0300

    style modal ok

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,26 +1,37 @@
 import Modal from 'react-modal'
 import { Container } from './style'
+import closeImg from '../../assets/close.svg'
 
 interface NewTransactionModalProps {
-    isOpen:boolean;
-    onRequestClose: ()=> void;
+    isOpen: boolean;
+    onRequestClose: () => void;
 }
 
-export function NewTransactionModal({isOpen,onRequestClose}:NewTransactionModalProps) {
+export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
     return (
         <Modal isOpen={isOpen}
-         onRequestClose={onRequestClose} 
-         overlayClassName='react-modal-overley'
-         className='react-modal-content'
-         
-         >
-            <h2>Cadastrar Transação</h2>
+            onRequestClose={onRequestClose}
+            overlayClassName='react-modal-overley'
+            className='react-modal-content'
+
+        >
+
             <Container>
+                <button type='button'
+                    onClick={onRequestClose}  >
+                    <img src={closeImg}
+                    alt="Fechar Modal" 
+                    className="react-modal-close"
+                    />
+                </button>
+
+                <h2>Cadastrar Transação</h2>
+
                 <form action="">
-                <input type="text" placeholder="Titulo" />
-                <input type="number" placeholder="Valor" />
-                <input type="text" placeholder="Categoria" />
-                <button type="submit">Cadastrar</button>
+                    <input type="text" placeholder="Titulo" />
+                    <input type="number" placeholder="Valor" />
+                    <input type="text" placeholder="Categoria" />
+                    <button type="submit">Cadastrar</button>
                 </form>
             </Container>
 
